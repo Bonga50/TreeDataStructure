a5c2d176983d623189ad1b0c563cdf8de0385110
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-10-24 07:42:09 +0000

    Added recursive function to print all the children

--- TreeDataStructure/BasicTree/Tester.cs
+++ TreeDataStructure/BasicTree/Tester.cs
@@ -37,21 +37,76 @@ namespace TreeDataStructure.BasicTree
 
             Tree brownTree = new Tree(new TreeNode(7));
             brownTree.root.children = new List<TreeNode>() { 
-            
+            new TreeNode(5){ parent = brownTree.root},
+            new TreeNode(8){ parent = brownTree.root}
             };
 
-            foreach (TreeNode item in myTree.root.children)
+            brownTree.root.children[0].children = new List<TreeNode>() {
+            new TreeNode(2){parent = brownTree.root.children[0]},
+            new TreeNode(3){parent = brownTree.root.children[0] },
+            new TreeNode(1){parent = brownTree.root.children[0] }
+            };
+
+
+            brownTree.root.children[1].children = new List<TreeNode>() {
+            new TreeNode(10){parent = brownTree.root.children[1]},
+            new TreeNode(12){parent = brownTree.root.children[1] }
+            };
+
+            brownTree.root.children[0].children[0].children = new List<TreeNode>() {
+            new TreeNode(13){parent = brownTree.root.children[0].children[0]},
+            new TreeNode(16){parent = brownTree.root.children[0].children[0] }
+            };
+
+
+            foreach (TreeNode item in brownTree.root.children)
             {
                 Console.WriteLine(item);
                 if (item.children.Count > 0)
                 {
-                    foreach (var item1 in item.children)
+                    foreach (TreeNode item1 in item.children)
                     {
                         Console.WriteLine("\t" + item1);
+
+                        if (item1.children.Count>0)
+                        {
+                            foreach (TreeNode item2 in item1.children)
+                            {
+                                Console.WriteLine("\t\t" + item2);
+                            }
+                        }
                     }
                 }
             }
+            Console.WriteLine("R"  );
+            PrintTreeRecursive(brownTree.root,0);
 
         }
+
+        public static void PrintTreeRecursive(TreeNode node, int level)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.parent != null)
+            {
+                Console.WriteLine(new string('\t', level) + node);
+            }
+            else
+            {
+                Console.WriteLine("Root"+ node.data);
+            }
+
+            //foreach (TreeNode child in node.children)
+            //{
+            //    PrintTreeRecursive(child, level + 1);
+            //}
+
+            node.children.ForEach(x => PrintTreeRecursive(x, level + 1));
+        }
+
+
     }
 }
