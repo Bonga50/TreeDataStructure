0cfbd5ae0f95ecd0dc9fd2a5e8484b759bf42e64
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-10-24 06:34:02 +0000

    Add project files.

---
+++ TreeDataStructure/BasicTree/Tester.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDataStructure.BasicTree
+{
+    internal class Tester
+    {
+        static void Main(string[] args)
+        {
+            #region Tree
+            Tree myTree = new Tree(new TreeNode(100));
+
+            TreeNode child1 = new TreeNode(50);
+            TreeNode child2 = new TreeNode(1);
+            TreeNode child3 = new TreeNode(150);
+            child1.parent = myTree.root; 
+            child2.parent = myTree.root;
+            child3.parent = myTree.root;
+
+            myTree.root.children.Add(child1);
+            myTree.root.children.Add(child2);
+            myTree.root.children.Add(child3);
+
+           
+
+            TreeNode child4 = new TreeNode(70);
+            TreeNode child5 = new TreeNode(61);
+            child4.parent = child2;
+            child5.parent = child2;
+
+            child2.children.Add(child4);
+            child2.children.Add(child5);
+            #endregion
+
+            Tree brownTree = new Tree(new TreeNode(7));
+            brownTree.root.children = new List<TreeNode>() { 
+            
+            };
+
+            foreach (TreeNode item in myTree.root.children)
+            {
+                Console.WriteLine(item);
+                if (item.children.Count > 0)
+                {
+                    foreach (var item1 in item.children)
+                    {
+                        Console.WriteLine("\t" + item1);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ TreeDataStructure/BasicTree/TreeNode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDataStructure.BasicTree
+{
+    internal class TreeNode
+    {
+        
+        public int data { get; set; }
+        public TreeNode parent { get; set; }
+        public List<TreeNode> children { get; set; }
+
+        public TreeNode(int data)
+        {
+            this.data = data;
+            children = new();      
+        }
+
+        public override string? ToString()
+        {
+            return $"Data child {data} parent: {parent.data} ";
+        }
+
+
+    }
+}
